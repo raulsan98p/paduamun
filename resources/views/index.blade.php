67a89be03845c90e091a1a702447e30532c80cf0
Raul <raulsanchezp1998@gmail.com>
2024-07-10 17:02:24 -0500

    hola

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,7 @@
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/estilosmodal.css">
+	<link rel="stylesheet" href="css/stylo.css">
 	
 	
 	<title>PADUAMUN 2024</title>
@@ -142,8 +143,6 @@
 	</header>
 
 		<!-- CARRUSEL SECTION INICIO -->
-	<section class="s-banner">
-		
 		<div class="swiper banner-swiper">
 			
 			<div class="swiper-wrapper">
