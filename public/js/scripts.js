2598d9e232da044ca69f09d0ddc813fb92a4b539
Raul <raulsanchezp1998@gmail.com>
2024-07-17 12:29:46 -0500

    traductor

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,32 @@
+
 function toggleText() {
+    if (espanol==true){
     const moreText = document.getElementById("more-text");
     const button = document.getElementById("toggle-button");
 
     if (moreText.classList.contains("hidden")) {
         moreText.classList.remove("hidden");
-        button.textContent = "Hide";
+        button.textContent = "Ocultar";
     } else {
         moreText.classList.add("hidden");
-        button.textContent = "Read more...";
+        button.textContent = "Leer mas...";
+    }
+} 
+
+    else {
+        const moreText = document.getElementById("more-text-en");
+        const button = document.getElementById("toggle-button-en");
+    
+        if (moreText.classList.contains("hidden")) {
+            moreText.classList.remove("hidden");
+            button.textContent = "Hide";
+        } else {
+            moreText.classList.add("hidden");
+            button.textContent = "Read more...";
+        }
+
     }
 }
+
+
+let espanol = true;
